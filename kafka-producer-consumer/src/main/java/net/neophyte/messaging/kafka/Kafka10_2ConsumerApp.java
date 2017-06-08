50804363c9ced4dfcb74df1120b703e3e8df308f
Shuvro Chakrobartty <Shuvro.Chakrobartty@walmart.com>
2017-06-08 18:20:45 -0500

    checking in producer, consumer changes

--- kafka-producer-consumer/src/main/java/net/neophyte/messaging/kafka/Kafka10_2ConsumerApp.java
+++ kafka-producer-consumer/src/main/java/net/neophyte/messaging/kafka/Kafka10_2ConsumerApp.java
@@ -2,6 +2,8 @@ package net.neophyte.messaging.kafka;
 
 import java.util.Arrays;
 
+import net.neophyte.messaging.kafka.utils.Util;
+
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
@@ -13,55 +15,87 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class Kafka10_2ConsumerApp extends AbstractKafkaClient {
-	private final Logger logger = LoggerFactory.getLogger(Kafka10_2ConsumerApp.class);
-	private final static String topicName = "test_topic";
-	private static long TEST_RUN_TIME = 1000 * 60 * 2;// two minutes
-	
+	private final static Logger logger = LoggerFactory.getLogger(Kafka10_2ConsumerApp.class);
+
 	public static void main(String[] args) {
 		Kafka10_2ConsumerApp kafkaConsumer = new Kafka10_2ConsumerApp();
 		try {
-			/* consumes at max 1000 messages, if available */
-			kafkaConsumer.receiveNumOfMessages(topicName, 1000);
-			/* consumes messages for TEST_RUN_TIME minutes */
-			kafkaConsumer.receiveMessagesFor(topicName, TEST_RUN_TIME);
+			/* consumes at max configured # of events, if available */
+			kafkaConsumer.receiveNumOfEvents(BROKER_CLUSTER.CLUSTER1, Configuration.getTopicName(), 
+					Configuration.getEventCount());
+			
+			/* consumes events for configured run time minutes */
+			kafkaConsumer.receiveEventsForDuration(BROKER_CLUSTER.CLUSTER1, Configuration.getTopicName(), 
+					Configuration.getRuntime());
 		}catch(Exception e) {
-			System.out.println(e.getMessage());
+			logger.error(e.getMessage());
 		}
 	}
 
-	private void receiveNumOfMessages(String topicName, int messageCount) throws Exception {
-		receiveMessages(topicName, messageCount, -1);
+	/**
+	 * Receive number of specified events
+	 * 
+	 * @param brokerCluster The broker Cluster
+	 * @param topicName The topic name
+	 * @param numOfEvents The number of events to produce
+	 * @throws Exception An exception in case of failure
+	 */
+	private void receiveNumOfEvents(BROKER_CLUSTER brokerCluster, String topicName, int numOfEvents) throws Exception {
+		receiveEvents(brokerCluster, topicName, numOfEvents, Configuration.IGNORE);
 	}
 
-	private void receiveMessagesFor(String topicName, long runTime) throws Exception {
-		receiveMessages(topicName, -1, runTime);
+	/**
+	 * Receive events for the specified run time
+	 * 
+	 * @param brokerCluster The broker Cluster
+	 * @param topicName The topic name
+	 * @param runTime The run time in mili seconds
+	 * @throws Exception An exception in case of failure
+	 */
+	private void receiveEventsForDuration(BROKER_CLUSTER brokerCluster, String topicName, long runTime) throws Exception {
+		receiveEvents(brokerCluster, topicName, Configuration.IGNORE, runTime);
 	}
 
-	private boolean moreMessagesToReceive(long messageCount) {
-		if (messageCount == -1) {
-			return true;
+	/**
+	 * 
+	 * @param numOfEvents The number of events to produce
+	 * @return <code>true</code> if not all events produced otherwise
+	 * <code>false</code>
+	 */
+	private boolean moreEventsToReceive(long numOfEvents) {
+		if (numOfEvents == Configuration.IGNORE) {
+			return false;
 		}
-		return msgs.get() < messageCount;
+		return msgs.get() < numOfEvents;
 	}
 	
-	public void receiveMessages(String topicName, int messageCount, long runTime) throws Exception {
+	/**
+	 * Receive events from kafka broker cluster on the specified topic
+	 * 
+	 * @param brokerCluster The broker Cluster
+	 * @param topicName The topic name
+	 * @param numOfEvents The number of events to produce
+	 * @param runTime The run time in mili seconds
+	 * @throws Exception An exception in case of failure
+	 */
+	public void receiveEvents(BROKER_CLUSTER brokerCluster, String topicName, int numOfEvents, long runTime) throws Exception {
 	    logger.info("Starting...");
         
 		startTime = System.currentTimeMillis();
 		
 	    KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(
-				getConsumerProperties(BROKER_CLUSTER.CLUSTER1, false, false));
+				getConsumerProperties(brokerCluster, false, false));
 	    
 	    consumer.subscribe(Arrays.asList(topicName));
 	    
-	    while(runTimeRemains(runTime) && moreMessagesToReceive(messageCount)) {
+	    while(runTimeRemains(runTime) || moreEventsToReceive(numOfEvents)) {
     	    try {
-    	        ConsumerRecords<String, String> records = consumer.poll(10000);
+    	        ConsumerRecords<String, String> records = consumer.poll(
+    	        		Configuration.getPolltimeout());
     	        msgs.addAndGet(records.count());
-    	        records.forEach(r -> System.out.println(" Msg: " + r.toString()));
     	        records.forEach(r -> System.out.println("Consumed from Partition: " 
     	        		+ r.partition() + " Offset: " + r.offset()
-    	        		+ " Msg: " + r.toString()));    	        
+    	        		+ " Msg: " + r.toString()));
     	        consumer.commitSync();
     	    }catch(Exception e){
     	        errors.incrementAndGet();
